f9159d7299de78b754a6c0eb466bfb2828d59041
itxviruscop <itxv.irus@gmail.com>
2025-03-29 15:35:43 +0500

    Update squotes.py

--- modules/squotes.py
+++ modules/squotes.py
@@ -1,19 +1,3 @@
-#  Moon-Userbot - telegram userbot
-#  Copyright (C) 2020-present Moon Userbot Organization
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import base64
 from io import BytesIO
 
@@ -66,7 +50,10 @@ async def quote_cmd(client: Client, message: Message):
         await message.delete()
         message = await client.send_message("me", "<b>Generating...</b>")
     else:
-        await message.edit("<b>Generating...</b>")
+        if message.from_user.is_self:
+            await message.edit("<b>Generating...</b>")
+        else:
+            await client.send_message(message.chat.id, "<b>Generating...</b>")
 
     url = "https://quotes.fl1yd.su/generate"
     params = {
@@ -135,7 +122,10 @@ async def fake_quote_cmd(client: Client, message: types.Message):
         await message.delete()
         message = await client.send_message("me", "<b>Generating...</b>")
     else:
-        await message.edit("<b>Generating...</b>")
+        if message.from_user.is_self:
+            await message.edit("<b>Generating...</b>")
+        else:
+            await client.send_message(message.chat.id, "<b>Generating...</b>")
 
     url = "https://quotes.fl1yd.su/generate"
     params = {
